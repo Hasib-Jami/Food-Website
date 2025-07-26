31ba27c1ab8fed1fe7bd00c06807aff93d36d65d
Hasib Jami <hasibullahjami55@gmail.com>
2025-07-26 08:36:18 +0430

    add script file

---
+++ script.js
@@ -0,0 +1,7 @@
+const sections=document.querySelectorAll('.animate-sec');
+const observer=new IntersectionObserver(entries=>{
+entries.forEach(e=>{
+    if(e.isIntersecting){ e.target.classList.add('show'); }
+});
+},{threshold:0.1});
+sections.forEach(s=>observer.observe(s));
