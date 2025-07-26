c5e7ffa286c25c6d16aefb39d265f375b85ecd54
Hasib Jami <hasibullahjami55@gmail.com>
2025-07-26 10:23:14 +0430

    add responsive header for size 768 px

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const sections=document.querySelectorAll('.animate-sec');
-const observer=new IntersectionObserver(entries=>{
-entries.forEach(e=>{
-    if(e.isIntersecting){ e.target.classList.add('show'); }
-});
-},{threshold:0.1});
-sections.forEach(s=>observer.observe(s));
+// const sections=document.querySelectorAll('.animate-sec');
+// const observer=new IntersectionObserver(entries=>{
+// entries.forEach(e=>{
+//     if(e.isIntersecting){ e.target.classList.add('show'); }
+// });
+// },{threshold:0.1});
+// sections.forEach(s=>observer.observe(s));
+
